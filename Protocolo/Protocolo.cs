34de9cbfe55485d618482cf6cc99f0f86cc84ed5
OscarSala98 <oscar.sala@epn.edu.ec>
2024-12-03 18:05:55 -0500

    Add files via upload

---
+++ Protocolo/Protocolo.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Protocolo
+{
+    public class Pedido
+    {
+        public string Comando { get; set; }
+        public string[] Parametros { get; set; }
+
+        public static Pedido Procesar(string mensaje)
+        {
+            var partes = mensaje.Split(' ');
+            return new Pedido
+            {
+                Comando = partes[0].ToUpper(),
+                Parametros = partes.Skip(1).ToArray()
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{Comando} {string.Join(" ", Parametros)}";
+        }
+    }
+
+    public class Respuesta
+    {
+        public string Estado { get; set; }
+        public string Mensaje { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Estado} {Mensaje}";
+        }
+    }
+
+    public class Protocolo
+    {
+        public string ResolverPedido(string mensajeRx, string direccionCliente, Dictionary<string, int> listadoClientes)
+        {
+            Pedido pedido = Pedido.Procesar(mensajeRx);
+            Respuesta respuesta = new Respuesta { Estado = "NOK", Mensaje = "Comando no reconocido" };
+
+            switch (pedido.Comando)
+            {
+                case "INGRESO":
+                    if (pedido.Parametros.Length == 2 &&
+                        pedido.Parametros[0] == "root" &&
+                        pedido.Parametros[1] == "admin20")
+                    {
+                        respuesta = new Random().Next(2) == 0
+                            ? new Respuesta { Estado = "OK", Mensaje = "ACCESO_CONCEDIDO" }
+                            : new Respuesta { Estado = "NOK", Mensaje = "ACCESO_NEGADO" };
+                    }
+                    else
+                    {
+                        respuesta.Mensaje = "ACCESO_NEGADO";
+                    }
+                    break;
+
+                case "CALCULO":
+                    if (pedido.Parametros.Length == 3)
+                    {
+                        string modelo = pedido.Parametros[0];
+                        string marca = pedido.Parametros[1];
+                        string placa = pedido.Parametros[2];
+                        if (ValidarPlaca(placa))
+                        {
+                            byte indicadorDia = ObtenerIndicadorDia(placa);
+                            respuesta = new Respuesta
+                            { Estado = "OK", Mensaje = $"{placa} {indicadorDia}" };
+                            ContadorCliente(direccionCliente, listadoClientes);
+                        }
+                        else
+                        {
+                            respuesta.Mensaje = "Placa no válida";
+                        }
+                    }
+                    break;
+
+                case "CONTADOR":
+                    if (listadoClientes.ContainsKey(direccionCliente))
+                    {
+                        respuesta = new Respuesta
+                        { Estado = "OK", Mensaje = listadoClientes[direccionCliente].ToString() };
+                    }
+                    else
+                    {
+                        respuesta.Mensaje = "No hay solicitudes previas";
+                    }
+                    break;
+            }
+
+            return respuesta.ToString();
+        }
+
+        private bool ValidarPlaca(string placa)
+        {
+            return Regex.IsMatch(placa, @"^[A-Z]{3}[0-9]{4}$");
+        }
+
+        private byte ObtenerIndicadorDia(string placa)
+        {
+            int ultimoDigito = int.Parse(placa.Substring(6, 1));
+            switch (ultimoDigito)
+            {
+                case 1:
+                case 2:
+                    return 0b00100000; // Lunes
+                case 3:
+                case 4:
+                    return 0b00010000; // Martes
+                case 5:
+                case 6:
+                    return 0b00001000; // Miércoles
+                case 7:
+                case 8:
+                    return 0b00000100; // Jueves
+                case 9:
+                case 0:
+                    return 0b00000010; // Viernes
+                default:
+                    return 0;
+            }
+        }
+
+        private void ContadorCliente(string direccionCliente, Dictionary<string, int> listadoClientes)
+        {
+            if (listadoClientes.ContainsKey(direccionCliente))
+            {
+                listadoClientes[direccionCliente]++;
+            }
+            else
+            {
+                listadoClientes[direccionCliente] = 1;
+            }
+        }
+
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Primer
-Primer Repositorio
+Resolución de la prueba acumulativa 01 de Aplicaciones Distribuidas.
+EL sistema distribuido consta de una aplicación cliente y un servidor. Se emplea el paradigma de paso de mensajes usando sockets.
+Se dispone de un protocolo basado en pedidos y respuestas.
